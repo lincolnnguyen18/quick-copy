7fb77e6b56236b01d21a94c6efdeefda3d49c41f
lincolnnguyen18 <letsmakeaworld@icloud.com>
2022-03-11 00:31:49 -0600

    done v1?

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 chrome.commands.onCommand.addListener(function (command) {
     switch (command) {
-        case 'duplicate-tab':
-            duplicateTab();
-            break;
         case 'bark':
             barkTitle();
             break;
@@ -10,16 +7,6 @@ chrome.commands.onCommand.addListener(function (command) {
             console.log(`Command ${command} not found`);
     }
 });
-
-/**
- * Gets the current active tab URL and opens a new tab with the same URL.
- */
-function duplicateTab() {
-    const query = { active: true, currentWindow: true };
-    chrome.tabs.query(query, (tabs) => {
-        chrome.tabs.create({ url: tabs[0].url, active: false });
-    });
-}
 
 function barkTitle() {
     const query = { active: true, currentWindow: true };
--- content.js
+++ content.js
@@ -1,45 +1,48 @@
 // Notification body.
-const notification = document.createElement("div");
-notification.className = 'acho-notification';
-
-// Notification icon.
-const icon = document.createElement('img');
-icon.src = chrome.runtime.getURL("images/icon32.png");
-notification.appendChild(icon);
+// const notification = document.createElement("div");
+// notification.className = 'acho-notification';
 
 // Notification text.
-const notificationText = document.createElement('p');
-notification.appendChild(notificationText);
+// const notificationText = document.createElement('p');
+// notification.appendChild(notificationText);
 
 // Add to current page.
-document.body.appendChild(notification);
+// document.body.appendChild(notification);
+
+let active = false;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    active = true;
 
     console.log(sender);
     console.log(request);
 
-    const notification = document.getElementsByClassName('acho-notification')[0];
-    const notificationText = notification.getElementsByTagName('p')[0];
+    // const notification = document.getElementsByClassName('acho-notification')[0];
+    // const notificationText = notification.getElementsByTagName('p')[0];
 
-    notificationText.innerHTML = "You are at: " + request.tabTitle;
+    // notificationText.innerHTML = "You are at: " + request.tabTitle;
 
-    notification.style.display = 'flex';
+    // notification.style.display = 'flex';
     
     console.log('test');
 
-    let oldBackground;
+    let oldBackground, currentHover;
 
     // print element cursor is over; add event listener to window
     window.addEventListener('mouseover', (e) => {
+        if (!active) return;
         let element = e.target;
+        currentHover = element;
         oldBackground = element.style.backgroundColor;
         element.style.backgroundColor = 'blue';
     });
 
     // remove border when not hovering over element
     window.addEventListener('mouseout', (e) => {
+        if (!active) return;
         let element = e.target;
+        currentHover = null;
         element.style.backgroundColor = oldBackground;
     });
 
@@ -69,6 +72,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     // log element on click
     window.addEventListener('click', (e) => {
+        if (!active) return;
         let element = e.target;
         let text = getVisibleText(element);
         text = reformatText(text);
@@ -76,7 +80,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         // copy text to clipboard
         navigator.clipboard.writeText(text).then(() => {
             alert("Text copied!");
+            active = false;
         });
+    });
+
+    // listen for escape key
+    window.addEventListener('keydown', (e) => {
+        if (e.key == 'Escape') {
+            if (active) {
+                active = false;
+                if (currentHover)
+                    currentHover.style.backgroundColor = oldBackground;
+            } else {
+                active = true;
+            }
+        }
     });
     
     return true;
