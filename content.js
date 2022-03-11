8417db414e68ab95e760abfcb489a65e62571b95
lincolnnguyen18 <letsmakeaworld@icloud.com>
2022-03-11 00:14:52 -0600

    update

--- content.js
+++ content.js
@@ -28,34 +28,56 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     
     console.log('test');
 
-    let oldBorder;
+    let oldBackground;
 
     // print element cursor is over; add event listener to window
     window.addEventListener('mouseover', (e) => {
         let element = e.target;
-        oldBorder = element.style.border;
-        element.style.border = '1px solid red';
-        // element.classList.add('hover');
-        // console.log(element);
+        oldBackground = element.style.backgroundColor;
+        element.style.backgroundColor = 'blue';
     });
 
     // remove border when not hovering over element
     window.addEventListener('mouseout', (e) => {
         let element = e.target;
-        element.style.border = oldBorder;
-        // element.classList.remove('hover');
-        // console.log(element);
+        element.style.backgroundColor = oldBackground;
     });
+
+    function getVisibleText(element) {
+        window.getSelection().removeAllRanges();
+        
+        let range = document.createRange();
+        range.selectNode(element);
+        window.getSelection().addRange(range);
+        
+        let visibleText = window.getSelection().toString().trim();
+        window.getSelection().removeAllRanges();
+        
+        return visibleText;
+    }
+
+    const reformatText = (text) => {
+        // replace all 1 or more newlines with 1 newline
+        text = text.replace(/\n{2,}/g, '\n');
+        // remove all leading and trailing whitespace from each line
+        let newtext = ""
+        text.split('\n').forEach((line) => {
+            newtext += line.trim() + '\n';
+        });
+        return newtext
+    }
 
     // log element on click
     window.addEventListener('click', (e) => {
         let element = e.target;
-        console.log(element);
+        let text = getVisibleText(element);
+        text = reformatText(text);
+        console.log(text);
+        // copy text to clipboard
+        navigator.clipboard.writeText(text).then(() => {
+            alert("Text copied!");
+        });
     });
-
-    // setTimeout(function () {
-    //     notification.style.display = 'none';
-    // }, 5000);
     
     return true;
 });
